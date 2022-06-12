24529b0d74a065193e17043784b1f91d0a7902d6
SQUIDzillaNFT <nrwisch@gmail.com>
2022-06-12 11:06:41 -0500

    add stake page

---
+++ stake/src/App.js
@@ -0,0 +1,40 @@
+import logo from './images/biglogo.png';
+import './App.css';
+import { Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+            <div>
+
+        </div>
+      
+        <div className="main-section">
+      
+        <img src={logo} className="logo-img" alt="logo" />
+        <h1>
+          Choose your Stake.
+        </h1>
+      <span>
+        <a className="tokenStake" href="https://mandoxstake.com" target="_blank" rel="noreferrer">
+          <Button>Token Staking</Button>
+        </a>
+        </span>
+        <span>
+        <a className="nftStake" href="https://MandoxNFTstake.com" target="_blank" rel="noreferrer">
+          <Button>NFT Staking</Button>
+        </a>
+        </span>
+
+        
+
+        </div>
+
+   
+    </div>
+    
+
+  );
+}
+
+export default App;
