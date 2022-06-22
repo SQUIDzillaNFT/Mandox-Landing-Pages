d951a97c14589cf546d1bd90f0f39267d58e8d5a
SQUIDzillaNFT <nrwisch@gmail.com>
2022-06-22 17:36:27 -0500

    size adjustments

--- stake/src/App.js
+++ stake/src/App.js
@@ -16,17 +16,37 @@ function App() {
         <h1>
           Choose your Stake.
         </h1>
-      <span>
+        <h2>Token Staking</h2>
+      
         <a className="tokenStake" href="https://mandoxstake.com" target="_blank" rel="noreferrer">
-          <Button>Token Staking</Button>
+          <Button>Mandox Staking</Button>
+        </a>
+        <div>
+          <h2>NFT Staking</h2>
+        <span>
+        <a className="nftStake" href="https://lacedameon.farm" target="_blank" rel="noreferrer">
+          <Button>Lacedameon</Button>
         </a>
         </span>
         <span>
-        <a className="nftStake" href="https://MandoxNFTstake.com" target="_blank" rel="noreferrer">
-          <Button>NFT Staking</Button>
+        <a className="nftStake" href="https://cyborx.farm" target="_blank" rel="noreferrer">
+          <Button>   The Cyborx   </Button>
         </a>
         </span>
-
+        </div>
+        <div className="soon">
+          <h2>Coming Soon...</h2>
+        <span>
+        <a className="bio" href="" target="_blank" rel="noreferrer">
+          <Button>Bio Apes</Button>
+        </a>
+        </span>
+        <span>
+        <a className="lost" href="" target="_blank" rel="noreferrer">
+          <Button>   Lost Kids   </Button>
+        </a>
+        </span>
+        </div>
         
 
         </div>
